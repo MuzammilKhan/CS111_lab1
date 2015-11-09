aad4b170f09f7def82c7f283de6b346c0fd3fdbf
Muzammil Ali Khan <muzammil@lnxsrv05.seas.ucla.edu>
2015-11-09 14:40:40 -0800

    slight update for design project

---
+++ testParallel.sh-23837.tmp/testtemp.sh
@@ -0,0 +1,17 @@
+
+cat words.txt
+
+cat words2.txt
+
+cat words.txt > words2.txt
+
+ls words.txt
+
+cat words2.txt
+
+echo hello3
+
+echo "quotation test" > quote.txt
+
+cat quote.txt
+
---
+++ testParallel.sh-27348.tmp/testtemp.sh
@@ -0,0 +1,19 @@
+
+cat words.txt
+
+cat words2.txt
+
+cat words.txt > words2.txt
+
+ls words.txt
+
+cat words2.txt
+
+echo hello3
+
+touch quote.txt
+
+echo quotation test > quote.txt
+
+cat quote.txt
+

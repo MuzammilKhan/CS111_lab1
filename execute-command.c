6b538bd49c35e5134f712f73137b6a05dee13dcd
Muzammil <muzammil.a.khan@live.com>
2015-11-10 11:21:05 -0800

    .

--- execute-command.c
+++ execute-command.c
@@ -62,12 +62,9 @@ increment_subprocess_count(int num_processes_needed)
     {
       while(*subprocess_count + num_processes_needed > *subprocess_limit) //busy loop till conditions are met
 	{
-    if(*subprocess_count + num_processes_needed <= *subprocess_limit)
-    {
-      pthread_mutex_lock(&mutex);
-    }
+    ;
   }
-      //pthread_mutex_lock(&mutex);
+      pthread_mutex_lock(&mutex);
       *subprocess_count += num_processes_needed;
       fprintf(stderr, "number of subprocesses after increment: %i\n", *subprocess_count);
       pthread_mutex_unlock(&mutex);
